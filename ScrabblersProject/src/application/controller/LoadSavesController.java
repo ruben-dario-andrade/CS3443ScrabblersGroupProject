997523a9d6491b9fd50b2dc985fbd3829bf33f57
Miguel Fabrigas <migfabrigas@gmail.com>
2020-11-27 20:03:16 -0600

    Give each save button on LoadSaves specific ID

--- ScrabblersProject/src/application/controller/LoadSavesController.java
+++ ScrabblersProject/src/application/controller/LoadSavesController.java
@@ -1,13 +1,16 @@
 package application.controller;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
+import java.util.ResourceBundle;
 import java.io.File;
 import java.io.FileOutputStream;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -15,18 +18,33 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class LoadSavesController {
+public class LoadSavesController implements Initializable{
 
 	@FXML
 	private AnchorPane mainPane;
 	@FXML
     private Button homeButton;
+	@FXML
+	private Button save1;
+	@FXML
+	private Button save2;
+	@FXML
+	private Button save3;	
+	@FXML
+	private Button save4;	
+	@FXML
+	private Button save5;
 
 	@FXML
 	public void initialize(){
 		
 	}
 	
+	/**
+	 * Redirects user to Main screen from LoadSaves screen
+	 * @param event ActionEvent triggered from pushing home button
+	 * @throws IOException
+	 */
 	@FXML
 	public void goHome(ActionEvent event) throws IOException{
 		mainPane = FXMLLoader.load(getClass().getResource("../view/Main.fxml"));
@@ -34,5 +52,28 @@ public class LoadSavesController {
 		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
 		window.setScene(scene);
 		window.show();
+	}
+	
+	/**
+	 * Loads save selected by user
+	 * @param event ActionEvent triggered from pushing one of the save buttons
+	 */
+	@FXML
+	public void loadSave(ActionEvent event) {
+		String selectedSave = ((Button)event.getSource()).getId();
+		System.out.println("Button clicked: " + selectedSave);
+	}
+	
+	/**
+	 * Initalizes button IDs
+	 */
+    @Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+    	//Set button IDs
+        save1.setId("save1");
+        save2.setId("save2");
+        save3.setId("save3");
+        save4.setId("save4");
+        save5.setId("save5");
 	}
 }
