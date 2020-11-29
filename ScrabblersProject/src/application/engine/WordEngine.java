4544a6456b0cf8aae076fa077279999294206e40
valeriaaviles <valeriaaviles@valerias-air>
2020-11-28 19:36:28 -0600

    Added some points output

--- ScrabblersProject/src/application/engine/WordEngine.java
+++ ScrabblersProject/src/application/engine/WordEngine.java
@@ -28,7 +28,8 @@ public class WordEngine {
 
 		System.out.println("The string at 6 is: " + str.charAt(6));
 		//createExclusiveList();
-		
+		int p = 'A';
+		System.out.println(p);
 		inputstring = str;
 		
 	}
@@ -51,35 +52,6 @@ public class WordEngine {
 		for (int i = 0; i < focus.size(); i++) {
 			focusLetter[i] = focus.get(i).charAt(0);
 			focusSize++;
-		}
-	}
-	
-	public void createInclusiveList() {
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader("res/ScrabbleDict.txt"));
-			String line = reader.readLine();
-			line = reader.readLine();
-			line = reader.readLine();
-			while (line != null) {
-				
-				
-				/*
-				boolean matchesCriteria = true;
-				for (int i = 0; i < focusSize; i++) {
-					if (line.indexOf(focusLetter[i]) == -1) {
-						matchesCriteria = false;
-					}
-				}
-				if (matchesCriteria) {
-					list.add(line);
-					System.out.println(line);
-				}
-				*/
-				line = reader.readLine();
-			}
-			reader.close();
-		} catch(Exception e) {
-			System.out.println("file not found");
 		}
 	}
 	
@@ -113,7 +85,7 @@ public class WordEngine {
 	
 	
 	public ArrayList getCurrentList() {
-		System.out.println(list.size());
+		System.out.println("list size is: " + list.size());
 		for (int i = 0; i < list.size(); i++) {
 			//System.out.println(list.get(i));
 		}
@@ -123,13 +95,13 @@ public class WordEngine {
 	static void printPermutation(String str, String ans) 
     { 
   
-		System.out.println("ran prmttn func");
+		//System.out.println("ran prmttn func");
         if (str.length() == 0) { 
         	//if(ans.charAt(0) == c) {
 	            pmt.add(ans); 
 	            //System.out.println(ans);
         	//}
-            return; 
+            return;
         } 
   
         for (int i = 0; i < str.length(); i++) { 
@@ -139,7 +111,7 @@ public class WordEngine {
             printPermutation(ros, ans + ch); 
         } 
     } 
-       
+      
     
     public void combine() { combine( 0 ); }
     private void combine(int start ){
@@ -158,7 +130,7 @@ public class WordEngine {
             output.setLength( output.length() - 1 );
         }
     }
-    
+
     public static void readFile () {
     	System.out.println("reading file rn, excuse me");
     	try {
@@ -171,6 +143,8 @@ public class WordEngine {
 	    		for(int i = 0; i < pmt.size(); i++) {
 	    			if(pmt.get(i).equals(s)) {
 	    				System.out.println("The word " + s + " is valid");
+	    				int count = points(s);
+	    				System.out.println("This word has " + count + " points");
 	    			}
 	            }
 	    		//System.out.println(s);
@@ -180,6 +154,39 @@ public class WordEngine {
 		{
 			System.out.println(e);
 		}
+    }
+    //10 q,z
+    //8 j,x
+    //5 k
+    //4 f,h,v,w,y
+    //3 b,c,m,p
+    //2 d, g
+    // 1 a,e,i,l,n,o,r,s,t,u
+    public static int points( String s ) {
+    	
+    	int points = 0;
+    	
+    	for(int i = 0; i < s.length(); i++) {
+    		
+    		if(s.charAt(i) == 'Q' || s.charAt(i) == 'Z' ) 
+    			points += 10;
+    		else if(s.charAt(i) == 'J' || s.charAt(i) == 'X' ) 
+    			points += 8;
+    		else if(s.charAt(i) == 'K') 
+    			points += 5;
+    		else if(s.charAt(i) == 'F' || s.charAt(i) == 'H' || s.charAt(i) == 'V' || s.charAt(i) == 'W' || s.charAt(i) == 'Y') 
+    			points += 4;
+    		else if(s.charAt(i) == 'B' || s.charAt(i) == 'C' || s.charAt(i) == 'M' || s.charAt(i) == 'P') 
+    			points += 3;
+    		else if(s.charAt(i) == 'D' || s.charAt(i) == 'G' ) 
+    			points += 2;
+    		else
+    			points += 1;
+    		    		
+    	}
+    	
+    	return points;
+    	
     }
 	
 }
