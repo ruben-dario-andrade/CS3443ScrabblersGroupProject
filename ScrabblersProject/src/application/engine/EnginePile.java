e031238b7c9c4c6e351fe383966c18bc8196482c
Miguel Fabrigas <migfabrigas@gmail.com>
2020-11-29 11:54:44 -0600

    Add debug comment for testing diff in save files

--- ScrabblersProject/src/application/engine/EnginePile.java
+++ ScrabblersProject/src/application/engine/EnginePile.java
@@ -22,7 +22,7 @@ public class EnginePile {
 	public EnginePile(LinkedList<String> savedPile) {
 		pileLetters = savedPile;
 		
-		Collections.shuffle(pileLetters);
+		Collections.shuffle(pileLetters); // comment out for testing diffs in save files
 	}
 
 	private static LinkedList<String> retrievePilePieces(){
