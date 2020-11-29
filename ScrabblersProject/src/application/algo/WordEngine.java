ef11a26dec322f6e6cfa8f74d21e22ae4e67897d
valeriaaviles <valeriaaviles@valerias-air>
2020-11-29 00:44:26 -0600

    first try

--- ScrabblersProject/src/application/algo/WordEngine.java
+++ ScrabblersProject/src/application/algo/WordEngine.java
@@ -1,20 +1,33 @@
+
 package application.algo;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
+
+import application.engine.GameEngine;
 
 public class WordEngine {
 	private char[] focusLetter = new char[15];
 	private short focusSize = 0;
 	private ArrayList<String> list = new ArrayList<String>();
 	BufferedReader reader;
-	public WordEngine() {
-		
-	}
+	private StringBuilder output = new StringBuilder();
+    private final String inputstring;
+    private static ArrayList<String> cmb = new ArrayList<String>();
+    private static ArrayList<String> pmt = new ArrayList<String>();
+    private static int wordLen;
+    private static char c;
 	
+	/*
+	public WordEngine() {
+		System.out.println("a ver");
+	}
+	*/
+
 	public void addList(LinkedList<String> focus) {
 		for (int i = 0; i < 15; i++) {
 			focusLetter[i] = ' ';
@@ -23,40 +36,44 @@ public class WordEngine {
 			focusLetter[i] = focus.get(i).charAt(0);
 		}
 		focusSize = (short) focus.size();
+		System.out.println("focus size is: " + focusSize);
 	}
 	
-	public void createInclusiveList() {
-		try {
-			this.reader = new BufferedReader(new FileReader("res/ScrabbleDict.txt"));
-			list.clear();
-			String line = this.reader.readLine();
-			line = this.reader.readLine();
-			line = this.reader.readLine();
-			while (line != null) {
-				boolean matchesCriteria = true;
-				for (int i = 0; i < focusSize; i++) {
-					if (line.indexOf(focusLetter[i]) == -1) {
-						matchesCriteria = false;
-					}
-				}
-				if (matchesCriteria) {
-					list.add(line);
-				}
-				line = this.reader.readLine();
-			}
-			this.reader.close();
-		} catch(Exception e) {
-			System.out.println("file not found");
-			e.printStackTrace();
-			try {
-				this.reader.close();
-			} catch (IOException e1) {
-				System.out.println("couldn't close");
-				e1.printStackTrace();
-			}
+	public WordEngine() {
+		
+		//System.out.println("Dimelo papi");
+		addList(GameEngine.getHand());
+		String str = new String(focusLetter);
+		//System.out.println("The current hand is: " + str);
+		//System.out.println("The string length is: " + str.length());
+		//System.out.println("The string at 6 is: " + str.charAt(6));
+		//createExclusiveList();
+		//int p = 'A';
+		//System.out.println("String is " + str.length() + " long");
+		String s = str.substring(0,focusSize);
+		//System.out.println("String is " + s.length() + " long");
+		inputstring = s;
+		
+	}
+	
+	public void main () {
+		c = 'a';
+		//System.out.println("inside main");
+		for(int i = 0; i < cmb.size(); i++) {  
+			printPermutation(cmb.get(i), "");
+        }
+        readFile();
+	}
+	
+
+	/*
+	public void addList(LinkedList<String> focus) {
+		for (int i = 0; i < focus.size(); i++) {
+			focusLetter[i] = focus.get(i).charAt(0);
+			focusSize++;
 		}
 	}
-	
+	*/
 	public void createExclusiveList() {
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader("res/ScrabbleDict.txt"));
@@ -84,6 +101,112 @@ public class WordEngine {
 		}
 	}
 	
+	
+	/*
+	public ArrayList getCurrentList() {
+		System.out.println("list size is: " + list.size());
+		for (int i = 0; i < list.size(); i++) {
+			//System.out.println(list.get(i));
+		}
+		return list;
+	}
+	*/
+	static void printPermutation(String str, String ans) 
+    { 
+  
+		//System.out.println("ran prmttn func");
+        if (str.length() == 0) { 
+        	//if(ans.charAt(0) == c) {
+	            pmt.add(ans); 
+	            //System.out.println(ans);
+        	//}
+            return;
+        } 
+  
+        for (int i = 0; i < str.length(); i++) { 
+            char ch = str.charAt(i); 
+            String ros = str.substring(0, i) + str.substring(i + 1); 
+
+            printPermutation(ros, ans + ch); 
+        } 
+    } 
+      
+    
+    public void combine() { combine( 0 ); }
+    private void combine(int start ){
+    	//System.out.println("ran combine func");
+    	//System.out.println(inputstring.length());
+        for( int i = start; i < inputstring.length(); ++i ){
+            output.append( inputstring.charAt(i) );
+            
+            String str = output.toString();
+            //System.out.println( str );
+            //if(str.length() == wordLen)
+            	cmb.add(str);
+            if ( i < inputstring.length() )
+            	combine( i + 1);
+            
+            output.setLength( output.length() - 1 );
+        }
+    }
+
+    public static void readFile () {
+    	System.out.println("reading file rn, excuse me");
+    	try {
+	    	File file = new File("res/ScrabbleDict.txt"); 
+	    	BufferedReader br = new BufferedReader(new FileReader(file)); 
+	    	
+	    	String s;
+	    	
+	    	while ((s = br.readLine()) != null) {
+	    		for(int i = 0; i < pmt.size(); i++) {
+	    			if(pmt.get(i).equals(s)) {
+	    				System.out.println("The word " + s + " is valid");
+	    				int count = points(s);
+	    				System.out.println("This word has " + count + " points");
+	    			}
+	            }
+	    		//System.out.println(s);
+	    	}
+	    	
+    	}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+    }
+    //10 q,z
+    //8 j,x
+    //5 k
+    //4 f,h,v,w,y
+    //3 b,c,m,p
+    //2 d, g
+    // 1 a,e,i,l,n,o,r,s,t,u
+    public static int points( String s ) {
+    	
+    	int points = 0;
+    	
+    	for(int i = 0; i < s.length(); i++) {
+    		
+    		if(s.charAt(i) == 'Q' || s.charAt(i) == 'Z' ) 
+    			points += 10;
+    		else if(s.charAt(i) == 'J' || s.charAt(i) == 'X' ) 
+    			points += 8;
+    		else if(s.charAt(i) == 'K') 
+    			points += 5;
+    		else if(s.charAt(i) == 'F' || s.charAt(i) == 'H' || s.charAt(i) == 'V' || s.charAt(i) == 'W' || s.charAt(i) == 'Y') 
+    			points += 4;
+    		else if(s.charAt(i) == 'B' || s.charAt(i) == 'C' || s.charAt(i) == 'M' || s.charAt(i) == 'P') 
+    			points += 3;
+    		else if(s.charAt(i) == 'D' || s.charAt(i) == 'G' ) 
+    			points += 2;
+    		else
+    			points += 1;
+    		    		
+    	}
+    	
+    	return points;
+    	
+    }
 	
 	
 	public LinkedList<String> getCurrentList() {
