946c052770ed87747ede2c849ac06b566da41fb1
Ruben Dario Andrade <rubyd.andrade@gmail.com>
2020-11-29 19:14:03 -0600

    fix stuff

--- ScrabblersProject/src/application/model/GameModel.java
+++ ScrabblersProject/src/application/model/GameModel.java
@@ -6,9 +6,13 @@ import application.engine.GameEngine;
 
 public class GameModel {
 	
+	public static int score;
+	
 	public static String endTurn() {
+		score = 0;
 		if(!GameEngine.getIsReplacing()) {
-			if (GameEngine.checkValid()) {
+			score = GameEngine.checkValid();
+			if (score != 0) {
 				GameEngine.getTray().clearHand();
 				GameEngine.refillHand();
 				GameEngine.resetTray();
